22c496524f38788515173ff6e52a6b457b58b3b8
Taewoong Moon <moondingie5@gmail.com>
2019-09-07 17:31:30 +0900

    expo Location , 날씨 api 연결

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 import Loading from './Loading';
+import * as Location from 'expo-location';
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Loading />
-    </View>
-  );
+const API_KEY = "c67c57a128e07d749b79758feece5d6f";
+
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  }
+
+  getWeather = async(latitude, longitude) => {
+    const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=c67c57a128e07d749b79758feece5d6f`);
+    console.log('data', data);
+  }
+
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      console.log(1, latitude, longitude)
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert('cant find yo', 'so sad')
+    }
+
+    
+  }
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
+  }
 }
 
 const styles = StyleSheet.create({
